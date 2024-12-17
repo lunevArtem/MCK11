9ed796df8ba8e4f2b974792a03494f4e06ce1dbd
lunevArtem <lunev2020artem@yandex.ru>
2024-12-17 03:31:38 +0300

    std::thread
    Добавлена реализации 6 функции через библиотеку std::thread

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -5,6 +5,9 @@
 #include <algorithm> // Для std::min_element и std::max_element
 #include <utility>   // Для std::pair
 #include <cmath>    // Для std::sqrt
+#include <thread>
+#include <chrono>
+
 
 // Функция для нахождения минимального и максимального элементов в вашем классе Vector
 std::pair<std::pair<int, size_t>, std::pair<int, size_t>> findMinMax(const Vector<int>& numbers) {
@@ -108,36 +111,91 @@ T dotProduct(const Vector<T>& vec1, const Vector<T>& vec2) {
     return result;
 }
 
+
+
+
+
+      // Параллельное выполнение функций с использованием потоков
+void parallelExecution(Vector<int>& vec1, Vector<int>& vec2) {
+   std::vector<std::thread> threads;
+
+   threads.emplace_back([&]() {
+        auto [minMaxPair1, minMaxPair2] = findMinMax(vec1);
+        auto [minValue, minIndex] = minMaxPair1;
+        auto [maxValue, maxIndex] = minMaxPair2;
+        std::cout << "Min value: " << minValue << " at index " << minIndex << "\n" << std::endl;;
+        std::cout << "Max value: " << maxValue << " at index " << maxIndex << "\n" << std::endl;;
+    });
+
+   threads.emplace_back([&]() {
+       double avg = calculateAverage(vec1);
+       std::cout << "Average value: " << avg << "\n"<< std::endl;;
+
+   });
+
+   threads.emplace_back([&]() {
+       double sum = SumOfElements(vec1);
+       std::cout << "Sum value: " << sum << "\n"<< std::endl;;
+
+   });
+
+   threads.emplace_back([&]() {
+       double manhattanNorm = calculateManhattanNorm(vec1);
+       std::cout << "Manhattan Norm: " << manhattanNorm << "\n"<< std::endl;;
+
+   });
+
+   threads.emplace_back([&]() {
+       double euclideanNorm = calculateEuclideanNorm(vec1);
+       std::cout << "Euclidean Norm: " << euclideanNorm << "\n"<< std::endl;;
+
+   });
+
+   threads.emplace_back([&]() {
+       double dotProd = dotProduct(vec1, vec2);
+       std::cout << "Dot Product: " << dotProd << "\n"<< std::endl;;
+
+   });
+
+   for (auto& th : threads) {
+       th.join(); // Ожидаем завершения всех потоков
+   }
+}
+
 int main() {
     Vector<int> vec(10); // Создаем вектор целых чисел с размером 10
-    Vector<int> vec1(100); // Создаем вектор целых чисел с размером 10
-    Vector<int> vec2(100); // Создаем вектор целых чисел с размером 10
+    Vector<int> vec1(100000000); // Создаем вектор целых чисел с размером 10
+    Vector<int> vec2(100000000); // Создаем вектор целых чисел с размером 10
 
     try {
-
+        // тут веткор с константными данными
+        /*
         //Константные значения вектора vec
         vec.initializeWithConstant(8); // Инициализируем все элементы значением 8
 
         std::cout << "Size of vector: " << vec.getSize() << std::endl;
         for (size_t i = 0; i < vec.getSize(); ++i) {
             std::cout << "Element " << i << ": " << vec[i] << std::endl;
-        }
+        }*/
 
         //Рандомные значения вектора vec1
-         vec1.initializeWithRandomNumbers(-10, 10); // Инициализируем случайными числами от -10 до 10
+         vec1.initializeWithRandomNumbers(-100, 100); // Инициализируем случайными числами от -10 до 10
         std::cout << "size of vector1: " << vec1.getSize() << std::endl;
         std::cout << "After inicialization random numbers vector1:" << std::endl;
+        /*
         for (size_t i = 0; i < vec1.getSize(); ++i) {
             std::cout << "Element " << i << ": " << vec1[i] << std::endl;
-        }
+        }*/
 
         //Рандомные значения вектора vec2
-         vec2.initializeWithRandomNumbers(-10, 11); // Инициализируем случайными числами от -10 до 10
+         vec2.initializeWithRandomNumbers(-100, 101); // Инициализируем случайными числами от -10 до 10
         std::cout << "size of vector2: " << vec2.getSize() << std::endl;
         std::cout << "After inicialization random numbers vector2:" << std::endl;
-        for (size_t i = 0; i < vec2.getSize(); ++i) {
+      /*  for (size_t i = 0; i < vec2.getSize(); ++i) {
             std::cout << "Element " << i << ": " << vec2[i] << std::endl;
         }
+
+        */
 
     } catch (const std::runtime_error& e) {
         std::cerr << "Error: " << e.what() << std::endl;
@@ -168,8 +226,19 @@ int main() {
         int ScalarComposition = dotProduct(vec1, vec2);
         std::cout << "Dot product of vector1 and vector2: " << ScalarComposition <<"\n"<< std::endl;
 
+    std::cout<< "---------------------------------------------------------"<<"\n" << std::endl;
+
+
+        parallelExecution(vec1, vec2);
+
+
+
+
+        /*
+
         // Экспорт данных в файл.
         vec1.exportToFile("output.txt");
+
         //Импорт данных из файла
         // Создаем новый объект для импорта.
         Vector<int> importedVec; // Создаем новый вектор
@@ -183,6 +252,7 @@ int main() {
             std::cout << "Element " << i << ": " << importedVec[i] << std::endl;
         }
 
+        */
     return 0;
 }
 
